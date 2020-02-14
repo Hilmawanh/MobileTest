eb3f500799c5671a996f7f930935103015d7f8fc
hilmawan <hilmawanhafidz4@gmail.com>
2020-02-14 11:29:34 +0700

    update

--- CobaEuy/App.js
+++ CobaEuy/App.js
@@ -7,97 +7,107 @@
  */
 
 import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {ScrollView, View, Text, Image, ActivityIndicator,Button} from 'react-native';
+import gaya from './src/component/gaya';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+var kalimat = 'Hello World!';
 
 const App = () => {
-  // const [nama] = useState(['nanai','hafos']);
-  // const [namateman] = useState('boso');
-  console.log('daebi');
-  // var nama = 'nanai';
-  // var namateman = 'boso';
+  const [nama] = useState(['david', 'dzaky', 'hafiz', 'aya', 'tika']);
+  const [namateman] = useState('bobi');
+  // const [jawaban,]
 
-  // const RenderText=()=>{
-  //   return nama.map((val,index)=>{
-  //     return(
-  //       <Text key={index} style={{color:"green",fontSize:20}}>HALO GAIS NAMA SAYA {val} dan nama teman saya {namateman} </Text>
-  //     )
-  //   })
-  // }
+  console.log('david');
+
+  const Rendertext = () => {
+    return nama.map((val, index) => {
+      return (
+        <Text style={gaya.tulisana} key={index}>
+          halo gais nama saya {val}
+        </Text>
+      );
+    });
+  };
+
 
   return (
     <>
-      {/* <View >
-        <Text style={styles.container}>
-          HALO GAIS NAMA SAYA {nama} , nama teman saya {namateman}
-          {RenderText()}
-          HALO GUYS!
-        </Text>
-      </View> */}
       <View
         style={{
-          // marginTop: 200,
           flexDirection: 'column',
           flex: 1,
         }}>
-        <View style={styles.kotak1}></View>
-        <View style={styles.kotak2}>
+        <View style={gaya.kotak1}>
+          <Text
+            style={{
+              marginTop: 50,
+              marginLeft: 120,
+              fontWeight: 'bold',
+              fontSize: 25,
+            }}>
+            INI HEADER
+          </Text>
+        </View>
+        <View style={gaya.kotak2}>
           <ScrollView>
-            <View style={{backgroundColor: 'gray', height: 200}}></View>
-            <View style={{backgroundColor: 'salmon', height: 210}}></View>
-            <View style={{backgroundColor: 'green', height: 200}}></View>
+            <View
+              style={{
+                backgroundColor: 'pink',
+                height: 200,
+                borderStyle: 'solid',
+                borderColor: 'blue',
+                borderWidth: 6,
+              }}>
+              <Image style={{width: 250, height: 250}} source={{uri :'https ://...barca.'}} />
+            </View>
+            <View style={{flexDirection: 'row', flex: 1, flexWrap: 'wrap'}}>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'black',
+                }}></View>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'gray',
+                }}></View>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'salmon',
+                }}></View>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'yellow',
+                }}></View>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'salmon',
+                }}></View>
+              <View
+                style={{
+                  height: 100,
+                  width: '33.3%',
+                  backgroundColor: 'gray',
+                }}></View>
+            </View>
+            <View style={{backgroundColor: 'whitesmoke', height: 210}}>
+              <ActivityIndicator size="large" color="black" />
+              <Text>{kalimat}</Text>
+            </View>
           </ScrollView>
         </View>
-        <View style={styles.kotak3}></View>
+        <View style={gaya.kotak3}></View>
       </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 300,
-    textAlign: 'center',
-    alignItems: 'center',
-    color: 'yellow',
-    fontSize: 25,
-    backgroundColor: 'pink',
-    padding: 15,
-    margin: 10,
-    fontWeight: 'bold',
-  },
-  kotak1: {
-    // width: 100,
-    // height: 100,
-    flex: 1,
-    backgroundColor: 'powderblue',
-  },
-  kotak2: {
-    // width: 100,
-    // height: 100,
-    flex: 6,
-    backgroundColor: 'skyblue',
-  },
-  kotak3: {
-    // width: 100,
-    // height: 100,
-    flex: 1,
-    backgroundColor: 'steelblue',
-  },
-});
 
 export default App;
--- CobaEuy/src/component/header.js
+++ CobaEuy/src/component/header.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import {View,Text} from 'react-native'
-import styles from './styles'
-
+import {
+    View,
+    Text
+} from 'react-native'
+import gaya from './gaya'
 class Header extends Component {
     state = {  }
     render() { 
-        return ( <View>
-
-        </View>);
+        return (
+            <View style={gaya.kotak1}>
+                
+            </View>
+          );
     }
 }
  
